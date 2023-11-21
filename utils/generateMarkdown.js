59b14e975c9dac2bfa77ceaadca906111536c47c
crypticsurfer <cb.connor.bishop@gmail.com>
2023-11-20 23:49:22 -0700

    added the functions that deal with license section link and badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (liscense !== "No Liscense") {
+        return `![Github Liscense](https://img.shields.io/badge/license-${license}-green.svg)`
+    }
+    return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === "No License") {
+        return `No License`
+      }
+      if (license === "MIT") {
+        return `* [License](https://opensource.org/license/mit/)`
+      }
+      if (license === "APACHE 2.0") {
+        return `* [License](https://www.apache.org/licenses/LICENSE-2.0)`
+      }
+      if (license === "GPL 3.0") {
+        return `* [License](https://www.gnu.org/licenses/gpl-3.0.en.html)`
+      }
+      return ""
+    
+    }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+        if (license !== "No License") {
+          return `This app was licensed under ${license} license`
+        }
+        return ""
+    }
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
